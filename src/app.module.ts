14063747ce356e0f9b7c1cf611669000041fe8c4
elephantMax <hatypovm12@mail.ru>
2024-05-24 22:53:54 +0300

    init users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { EnvModule } from './env/env.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, EnvModule],
   controllers: [AppController],
   providers: [AppService],
 })
