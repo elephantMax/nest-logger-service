4208e4a2c426519b685f734d02d61ecfbe079681
elephantMax <hatypovm12@mail.ru>
2024-07-07 21:13:08 +0300

    fix validation

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -2,7 +2,7 @@ import { createZodDto } from 'nestjs-zod';
 import { z } from 'nestjs-zod/z';
 
 const CreateProjectSchema = z.object({
-  name: z.string(),
+  name: z.string().min(3),
 });
 
 export class CreateProjectDto extends createZodDto(CreateProjectSchema) {}
