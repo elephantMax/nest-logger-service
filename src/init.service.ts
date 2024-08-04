91029c0512692cc1edf10a746cc79a2e5f5d799d
elephantMax <hatypovm12@mail.ru>
2024-08-04 19:14:59 +0300

    fix auth service

--- src/init.service.ts
+++ src/init.service.ts
@@ -1,19 +1,35 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { AuthService } from './auth/auth.service';
+import { EnvService } from './env/env.service';
+import { CreateUserDto } from './shared/dto';
 
 @Injectable()
 export class InitService implements OnModuleInit {
   constructor(
     private readonly authService: AuthService,
+    private readonly envService: EnvService,
     private readonly logger: Logger,
   ) {}
 
   async onModuleInit() {
     try {
-      await this.authService.initAdmin();
+      await this.initAdmin();
       this.logger.log('Admin was successfully created');
     } catch (error) {
       this.logger.error('failed to create Admin', error);
     }
+  }
+
+  private async initAdmin() {
+    const adminLogin = this.envService.get('ADMIN_LOGIN');
+    const adminPassword = this.envService.get('ADMIN_PASSWORD');
+
+    const adminDto: CreateUserDto = {
+      login: adminLogin,
+      password: adminPassword,
+      name: 'admin',
+    };
+
+    return this.authService.register(adminDto);
   }
 }
